5764ae3f3e33dd960caf7399277d834b881ddfff
tungle203 <thanhtunga52021@gmail.com>
2023-04-24 11:04:21 +0700

    Xây dựng UI phần sidebar #1

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,11 +2,12 @@ import config from '~/config';
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
-
+import Live from '~/pages/Live/Live';
 const publicRoutes = [
   { path: config.routes.home, component: Home },
   { path: config.routes.profile, component: Profile },
   { path: config.routes.following, component: Following },
+  { path: config.routes.live, component: Live },
 ];
 const privateRoutes = [];
 export { publicRoutes, privateRoutes };
---
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -0,0 +1,31 @@
+import config from '~/config';
+import styles from './Sidebar.module.scss';
+import classNames from 'classnames/bind';
+
+import {
+  HomeIcon,
+  HomeActiveIcon,
+  UserGroupIcon,
+  UserGroupActiveIcon,
+  LiveIcon,
+  LiveActiveIcon,
+} from '~/components/Icons';
+import Menu, { MenuItem } from './Menu';
+const cx = classNames.bind(styles);
+function Sidebar() {
+  return (
+    <aside className={cx('wrapper')}>
+      <Menu>
+        <MenuItem title="For You" to={config.routes.home} icon={<HomeIcon />} activeIcon={<HomeActiveIcon />} />
+        <MenuItem
+          title="Following"
+          to={config.routes.following}
+          icon={<UserGroupIcon />}
+          activeIcon={<UserGroupActiveIcon />}
+        />
+        <MenuItem title="Live" to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
+      </Menu>
+    </aside>
+  );
+}
+export default Sidebar;
